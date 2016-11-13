3b2650190ab180213835bd8ab23bb917be57a825
Jason Miller <jason@redconfetti.com>
2016-11-13 15:29:45 -0800

    Organizes code into directories named by chapter sections

--- 1.1.0-hello-world/main.c
+++ 1.1.0-hello-world/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+// To compile this use:
+// $ cc -o hello main.c
 int main(int arg, char *argv[]) {
   printf("hello, world\n");
   return 0;
--- 1.5.2.a-character-counting/main.c
+++ 1.5.2.a-character-counting/main.c
@@ -2,10 +2,13 @@
 
 /*
   The normal Unix convention is that Control-D is used to indicate end-of-file (EOF).
+
+  See misc/getchar-test for inspecting the getchar function
 */
 int main(int arg, char *argv[]) {
   long nc;
   nc = 0;
+
   while (getchar() != EOF)
     ++nc;
   printf("%ld\n", nc);
