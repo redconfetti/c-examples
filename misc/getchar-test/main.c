e0f9d3e048dea43abf81e1b3e52974b99d4e36bc
Jason Miller <jason@redconfetti.com>
2016-11-15 22:00:53 -0800

    Adds test script for misc testing

--- misc/getchar-test/main.c
+++ misc/getchar-test/main.c
@@ -4,15 +4,14 @@
   Demonstrates how only a single character is captured at a time
   by getchar()
 */
-int main ()
-{
-   char c;
- 
-   printf("Enter character: ");
-   c = getchar();
- 
-   printf("Character entered: ");
-   putchar(c);
+int main() {
+  char c;
 
-   return(0);
+  printf("Enter character: ");
+  c = getchar();
+
+  printf("Character entered: ");
+  putchar(c);
+
+  return(0);
 }
